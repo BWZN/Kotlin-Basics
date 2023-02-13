21a5e14130773bb3a9c14bc3350906db84a9ddf8
Alexander Pak <bwazennnn@gmail.com>
2023-02-13 18:19:11 +0600

    Commit

--- app/src/main/java/com/example/kotlinbasics/BasicsPart2.kt
+++ app/src/main/java/com/example/kotlinbasics/BasicsPart2.kt
@@ -3,7 +3,37 @@ package com.example.kotlinbasics
 fun main(){
     // Argument
     var avg = average(5.3, 13.13)
-    print("result is $avg")
+    /*print("result is $avg")
+
+     */
+
+    // Nullables
+
+    // name = null -> Compilation Error
+    var nullableName : String? = "Alexander"
+    //nullableName = null
+
+    var len2 = nullableName?.length
+    //nullableName?.let { println(it.length) }
+    /*
+    if(nullableName != null) {
+        var len2 = nullableName.length
+    }else{
+        null
+    }
+
+     */
+
+    // ?: Elvis Operator, Not Null Assertion
+    val name = nullableName ?: "Guest"
+    println("name is $name")
+
+    println(nullableName!!.toLowerCase())
+
+
+
+
+
 }
 // Method - a Method is a Function within a class
 
